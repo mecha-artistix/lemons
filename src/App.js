f7a0d6d6d981d9aaf256faf5e510c2f9e712822d
Huzaifa Bin Abdul Qayyum <mecha.artstix@gmail.com>
2023-03-04 13:01:22 +0500

    change to master

--- src/App.js
+++ src/App.js
@@ -15,7 +15,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          New Branch
+          Master Branch change
         </a>
       </header>
     </div>
